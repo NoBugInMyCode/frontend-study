8d4d2cf86c2e9da1c7c3d80efceaf000d532dfbb
NoBugInMyCode <knysun@ucdavis.edu>
2023-04-02 02:33:20 -0700

    Items added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
 import logo from './logo.svg';
+import banana from './banana.jpeg'
 import './App.css';
+import { FontSizeOutlined, SearchOutlined } from '@ant-design/icons';
+import { Layout, Space } from 'antd';
+
+
+const { Sider, Header, Content, Footer } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'left',
+  color: 'black',
+  height: 64,
+  paddingInline: '5000',
+  lineHeight: '64px',
+  backgroundColor: 'white',
+  minWidth: '5000px',
+  
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: 'white',
+};
+
+
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'left',
+  color: 'black',
+  height: 64,
+  paddingInline: '5000',
+  lineHeight: '64px',
+  backgroundColor: 'white',
+  minWidth: '5000px',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'left',
+  color: 'black',
+  backgroundColor: 'white',
+  
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='webHeader'>我的订单</header>
+      <Layout>
+
+      <Sider style={siderStyle} className="img"><img src={banana} className="img"></img></Sider>
+        <Layout>
+          <Header style={headerStyle}>
+            <p className='itemName'>香蕉</p>
+          </Header>
+          <Content style={contentStyle}>
+            一把香蕉
+          </Content>
+          <Footer style={footerStyle}>
+            $5
+          </Footer>
+        </Layout>
+
+      
+      
+      </Layout>
+
+
+      <Layout>
+
+        <Sider style={siderStyle} className="img"><img src={banana} className="img"></img></Sider>
+        <Layout>
+          <Header style={headerStyle}>
+            <p className='itemName'>香蕉</p>
+          </Header>
+          <Content style={contentStyle}>
+            一把香蕉
+          </Content>
+          <Footer style={footerStyle}>
+            $5
+          </Footer>
+  </Layout>
+
+
+
+</Layout>
+
+
+
+
+      {/* <header className="App-header-1">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. aaaaaaa
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to google
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
