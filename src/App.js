9a252c505f4d931e44512ab02d022ac62eca5f4a
RaulZuo <zuobin_nju@163.com>
2023-04-04 00:30:37 +0800

    fix: fix render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import banana from './banana.jpeg';
-import kiwi from './kiwi.jpg';
-import apple from './apple.jpg'
+// import banana from './banana.jpeg';
+// import kiwi from './kiwi.jpg';
+// import apple from './apple.jpg'
 import './App.css';
 import { Layout, Button,Divider, Input, Rate } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
+import { useEffect, useState } from 'react';
 
 
 const { Sider, Content } = Layout;
@@ -31,14 +32,98 @@ function apear(id) {
   document.getElementById(id).style.display = 'flex'
 }
 
+// 先定义从服务器获取数据，这是一个本地 Mock 的数据
+function getDateSource() {
+  return [
+    {
+      image: '/banana.jpeg',
+      name: '香蕉',
+      description: '一把香蕉',
+      price: '$5',
+      rate: 1,
+      comment: '这个香蕉非常甜',
+    },
+    {
+      image: '/apple.jpg',
+      name: '苹果',
+      description: '一筐苹果',
+      price: '$8',
+    },
+    {
+      image: '/kiwi.jpg',
+      name: '猕猴桃',
+      description: '两个猕猴桃',
+      price: '$2',
+    }
+  ];
+}
+
 function App() {
+  // 定义 App 这个组件内部维护的数据，使用 useState 本地维护一份数据，初始数据为一个空数组
+  const [dataSource, setDataSource] = useState([]);
+
+  // App 组件初始化后，调用 getDataSouce 获取远程数据
+  useEffect(() => {
+    // 获取远程数据
+    const data = getDateSource();
+    // 将数据更新到组件内部维护的数据中
+    setDataSource(data);
+  }, []);
+
+  // 定义一个函数，来开启或者关闭评论部分
+  const showComment = (index) => {
+    const data = dataSource[index];
+    if (!data) {
+      return;
+    }
+    data.show = !data.show;
+    // 更新数据，由于dataSource 是一个数组，是复杂数据结构，所以需要重新生成
+    setDataSource(dataSource.map(data => ({ ...data })));
+  }
+
   return (
     <div className="App">
       <header className='webHeader'>我的订单</header>
+      {
+        dataSource.map((data, index) => {
+          return (
+            <div key={index}>
+              <Layout>
+                <Sider style={siderStyle} className="img"><img src={data.image}className="img"></img></Sider>
+                <Layout>
+                  <Content style={contentStyle}>
+                    <b>{data.name}</b>
+                  </Content>
+                  <Content style={contentStyle}>
+                    {data.description}
+                  </Content>
+                  <Content style={contentStyle}>
+                    {data.price}
+                    <Button id='bananaButton' style={{float:"right", margin:10}} onClick={() => showComment(index)}>
+                      { data.rate ? '已评价' : '评价' }
+                    </Button>
+                  </Content>
+                  {
+                    data.show ? (
+                      <div>
+                        <TextArea autoSize="true" className='inputBoxBananaxxx' id='bananaInputxx' value={data.comment}></TextArea>
+                        <Rate className='rateBoxBananasss' id = 'rateBanana' value={data.rate}></Rate>
+                      </div>
+                    ) : null
+                  }
+                    {/* <TextArea autoSize="true" className='inputBoxBanana' id='bananaInput'></TextArea>
+                    <Rate className='rateBoxBanana' id = 'rateBanana'></Rate> */}
+                </Layout>
+              </Layout>
+              { index !== data.length ?  <Divider /> : null }
+            </div>
+          );
+        })
+      }
 
 
 
-      <Layout>
+      {/* <Layout>
         <Sider style={siderStyle} className="img"><img src={banana} className="img"></img></Sider>
           <Layout>
             <Content style={contentStyle}>
@@ -97,7 +182,7 @@ function App() {
             <TextArea autoSize="true" className='inputBoxKiwi'></TextArea>
             <Rate className='rateBoxKiwi'></Rate>
           </Layout>
-      </Layout>
+      </Layout> */}
 
 
 
